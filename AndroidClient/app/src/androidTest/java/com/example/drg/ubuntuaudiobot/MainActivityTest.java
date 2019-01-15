faf7b6587706e5f6fde7d31d09081906fd4cae4c
digishgabhawala <digish_gabhawala@yahoo.com>
2019-01-15 23:19:07 +0530

    added automation tests

--- AndroidClient/app/src/androidTest/java/com/example/drg/ubuntuaudiobot/MainActivityTest.java
+++ AndroidClient/app/src/androidTest/java/com/example/drg/ubuntuaudiobot/MainActivityTest.java
@@ -1,20 +1,39 @@
 package com.example.drg.ubuntuaudiobot;
 
 import android.support.test.espresso.Espresso;
+import android.support.test.espresso.ViewInteraction;
+import android.support.test.filters.LargeTest;
 import android.support.test.rule.ActivityTestRule;
+import android.support.test.runner.AndroidJUnit4;
+import android.view.View;
+import android.view.ViewGroup;
+import android.view.ViewParent;
 
+import org.hamcrest.Description;
+import org.hamcrest.Matcher;
+import org.hamcrest.TypeSafeMatcher;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Rule;
 import org.junit.Test;
+import org.junit.runner.RunWith;
 
+import static android.support.test.espresso.Espresso.onView;
 import static android.support.test.espresso.action.ViewActions.click;
+import static android.support.test.espresso.action.ViewActions.closeSoftKeyboard;
+import static android.support.test.espresso.action.ViewActions.pressImeActionButton;
+import static android.support.test.espresso.action.ViewActions.replaceText;
 import static android.support.test.espresso.action.ViewActions.typeText;
 import static android.support.test.espresso.assertion.ViewAssertions.matches;
+import static android.support.test.espresso.matcher.ViewMatchers.isDisplayed;
+import static android.support.test.espresso.matcher.ViewMatchers.isRoot;
 import static android.support.test.espresso.matcher.ViewMatchers.withId;
 import static android.support.test.espresso.matcher.ViewMatchers.withText;
+import static org.hamcrest.Matchers.allOf;
 import static org.junit.Assert.*;
 
+@LargeTest
+@RunWith(AndroidJUnit4.class)
 public class MainActivityTest {
 
     @Rule
@@ -42,4 +61,116 @@ public class MainActivityTest {
         Espresso.onView(withId(R.id.result)).check(matches(withText("Result : 5")));
 
     }
+
+//    @Test
+//    public void mainActivityTest3() {
+//        ViewInteraction appCompatEditText = onView(
+//                allOf(withId(R.id.texta), withText("0"),
+//                        childAtPosition(
+//                                childAtPosition(
+//                                        withId(android.R.id.content),
+//                                        0),
+//                                2),
+//                        isDisplayed()));
+//        appCompatEditText.perform(replaceText("55580"));
+//
+//        ViewInteraction appCompatEditText2 = onView(
+//                allOf(withId(R.id.texta), withText("55580"),
+//                        childAtPosition(
+//                                childAtPosition(
+//                                        withId(android.R.id.content),
+//                                        0),
+//                                2),
+//                        isDisplayed()));
+//        appCompatEditText2.perform(closeSoftKeyboard());
+//
+//        ViewInteraction appCompatEditText3 = onView(
+//                allOf(withId(R.id.textb), withText("0"),
+//                        childAtPosition(
+//                                childAtPosition(
+//                                        withId(android.R.id.content),
+//                                        0),
+//                                1),
+//                        isDisplayed()));
+//        appCompatEditText3.perform(replaceText("3"));
+//
+//        ViewInteraction appCompatEditText4 = onView(
+//                allOf(withId(R.id.textb), withText("3"),
+//                        childAtPosition(
+//                                childAtPosition(
+//                                        withId(android.R.id.content),
+//                                        0),
+//                                1),
+//                        isDisplayed()));
+//        appCompatEditText4.perform(closeSoftKeyboard());
+//
+//        ViewInteraction appCompatEditText5 = onView(
+//                allOf(withId(R.id.textb), withText("3"),
+//                        childAtPosition(
+//                                childAtPosition(
+//                                        withId(android.R.id.content),
+//                                        0),
+//                                1),
+//                        isDisplayed()));
+//        appCompatEditText5.perform(pressImeActionButton());
+//
+//        ViewInteraction appCompatEditText6 = onView(
+//                allOf(withId(R.id.texta), withText("55580"),
+//                        childAtPosition(
+//                                childAtPosition(
+//                                        withId(android.R.id.content),
+//                                        0),
+//                                2),
+//                        isDisplayed()));
+//        appCompatEditText6.perform(pressImeActionButton());
+//
+//        ViewInteraction appCompatEditText7 = onView(
+//                allOf(withId(R.id.textb), withText("3"),
+//                        childAtPosition(
+//                                childAtPosition(
+//                                        withId(android.R.id.content),
+//                                        0),
+//                                1),
+//                        isDisplayed()));
+//        appCompatEditText7.perform(pressImeActionButton());
+//
+//        appCompatEditText.perform(click());
+//
+//        appCompatEditText3.perform(click());
+//
+//        appCompatEditText.perform(click());
+//
+//
+//
+//        ViewInteraction textView = onView(
+//                allOf(withId(R.id.result), withText("Result : 55583"),
+//                        childAtPosition(
+//                                childAtPosition(
+//                                        withId(android.R.id.content),
+//                                        0),
+//                                4),
+//                        isDisplayed()));
+//
+//
+//        textView.check(matches(withText("Result : 55583")));
+//    }
+//
+//    private static Matcher<View> childAtPosition(
+//            final Matcher<View> parentMatcher, final int position) {
+//
+//        return new TypeSafeMatcher<View>() {
+//            @Override
+//            public void describeTo(Description description) {
+//                description.appendText("Child at position " + position + " in parent ");
+//                parentMatcher.describeTo(description);
+//            }
+//
+//            @Override
+//            public boolean matchesSafely(View view) {
+//                ViewParent parent = view.getParent();
+//                return parent instanceof ViewGroup && parentMatcher.matches(parent)
+//                        && view.equals(((ViewGroup) parent).getChildAt(position));
+//            }
+//        };
+//    }
 }
